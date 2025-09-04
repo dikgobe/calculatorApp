243720dc70dafc886997c3ed7f211945faba59f9
dikgobe <djmolepo@gmail.com>
2025-09-04 07:39:18 +0200

    fixed error in views

--- mycalculator/views.py
+++ mycalculator/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def calculator_view(request):
+		results= None
+		error =None	
+		if request.method == 'POST':
+			try:
+				num1 = float(request.POST.get('num1'))
+				num2= float(request.POST.get('num2'))
+				operation = request.POST .get('operation')
+
+				if operation  == "add":
+					results	= num1	+ num2
+				elif operation	== "subraction":
+					results = num2 - num1
+				elif operation	== 'division':
+					results	= num2/num1
+				elif operation	=='multiply':
+					results = num1 * num2
+				else:
+					error = 'invalid error'
+			except ValueError:
+				error = 'wrong value'
+			except ZeroDivisionError:
+				error = "Cant devide by zero"
+		return render(request,'mycalculator/index.html',{ "results"	: results	, "error ": error	 })
